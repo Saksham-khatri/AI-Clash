894358b39ab6748bf3742d7ade9b86340b5c1b79
Saksham <sakshamkhatri771@gmail.com>
2026-08-25 14:23:16 +0530

    Fix production CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,7 @@ import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-clash.vercel.app",
     methods:['GET','POST'],
     credentials: true
 }))
